47461750141fea727e29292cd5f0484ace522a88
muratkistan <muratkistan@gmail.com>
2022-09-09 19:41:28 +0300

    First Commit

---
+++ src/main/java/com/muratkistan/ft/FtTeknolojiPracticumApplication.java
@@ -0,0 +1,13 @@
+package com.muratkistan.ft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtTeknolojiPracticumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtTeknolojiPracticumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muratkistan/ft/FtTeknolojiPracticumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muratkistan.ft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtTeknolojiPracticumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
